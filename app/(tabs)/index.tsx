aa8c9f73dcaabf45203297cfa5afc26c21650564
DJOSSOU Comlan Ulrich <uldjoss56@gmail.com>
2024-07-04 15:30:54 +0100

    changes name

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome Ulrich!</ThemedText>
+        <ThemedText type="title">Welcome Ulrich DDDDDDJSOSOSOU!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
